6f343a77435cd90e905eb4598733e50073882a1a
Venkatesh <venky.chowdary91@gmail.com>
2018-09-25 13:56:41 +0200

    Change titles and styling on homepage

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+var port = process.env.PORT || 8080;
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/taxiWala'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/taxiWala/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(port, function() {
+  console.log("listening on port 5000")
+});
